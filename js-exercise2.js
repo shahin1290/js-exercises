cc2ba183d0bca107188d60bec9143d6502462174
Aditya Naik <naik.aditya@gmail.com>
2018-11-05 15:00:45 +0100

    Solution for exercise #2

---
+++ js-exercise2.js
@@ -0,0 +1,25 @@
+//Q: What is 'this'? Does it have an equivalent in Ruby?
+
+//A: 'this' in JS allows us to access the instance variable. This is like 'self' in Ruby.
+
+//The above Person class is written with ES5, now that we've learned about ES2015, convert the above code class to an ES2015 version using the new class syntax
+
+class Person {
+
+    constructor(first,last){
+      this.first = first
+      this.last = last
+    }
+  
+    setName(){
+  
+      console.log(`My full name is ${this.first} ${this.last}`)
+  
+    }
+  
+  
+  }
+  
+  bob = new Person('Thomas','Ochman')
+  
+  bob.setName()
